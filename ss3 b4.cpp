d0439fc6c8575691b93ea03e89cfb619b675eee0
hongky06 <dohongky2006@gmail.com>
2024-11-21 17:45:45 +0700

    Add files via upload

---
+++ ss3 b4.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+	float toan,van,anh,total,average;
+	printf("nhap lan luot diem toan van anh : ");
+	scanf ("%f %f %f",&toan,&van,&anh);
+	total = toan + van + anh;
+	average = total / 3;
+	printf ("tong = %.2f \n",total),
+	printf ("trung binh = %.2f \n",average); 
+	 
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
