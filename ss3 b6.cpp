f965ad9723c303bc452ba810883c861b33725f15
hongky06 <dohongky2006@gmail.com>
2024-11-21 18:00:41 +0700

    Add files via upload

---
+++ ss3 b6.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main (){
+	float chieu_dai_cach,chieu_cao,dien_tich;  
+	printf("hay nhap chieu dai = ");
+	scanf("%f",&chieu_dai_cach); 
+	printf("hay nhap chieu cao = ");
+	scanf("%f",&chieu_cao); 
+	dien_tich = (chieu_dai_cach *  chieu_cao) / 2;
+	printf ("dien tich = %.2f",dien_tich); 
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0; 
+} 
