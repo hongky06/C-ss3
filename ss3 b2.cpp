ee5a0fb3d553c26b32d2c80b51972ed14c9286c3
hongky06 <dohongky2006@gmail.com>
2024-11-21 17:05:44 +0700

    Add files via upload

---
+++ ss3 b2.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+  int main(){
+  
+  float do_c,do_f;
+  printf("nhap do c =");
+  scanf("%f",&do_c);
+  do_f = (float)do_c * 1.8 + 32 ;
+  printf("do f la: %.2f \n",do_f);
+    
+  
+  
+   return 0;
+  
+  
+  }
