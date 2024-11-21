19de006e7b24a4baf0d8732cd39e0d287f0c4d18
hongky06 <dohongky2006@gmail.com>
2024-11-21 17:30:43 +0700

    Add files via upload

---
+++ ss3 b3.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	float r ,chu_vi,dien_tich;
+	float pi = 3.14;
+	printf("nhap ban kinh = ");
+	scanf ("%f",&r);
+	chu_vi =  2 * pi * r ;
+	dien_tich = pi * r * r;
+	printf("chu vi %f \n ",chu_vi);
+	printf("dien tich %f \n",dien_tich);
+	
+	
+	
+	
+	
+	return 0;
+}
